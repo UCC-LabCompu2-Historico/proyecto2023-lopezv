f84ebe54db15755f6cc4711e9d7bbe7afb671ef5
aguslopez9 <agustinlopezv99@gmail.com>
2023-05-24 16:45:00 -0300

    compara palabras y carteles

--- funcionesJs.js
+++ funcionesJs.js
@@ -1,24 +1,22 @@
-
-let iguales = 0;
 let n = 0;
-const palabras = ["Amigo", "Aviso", "Ancho", "Arena", "Asado",
-    "Banco", "Banda", "BARCO", "Bolsa", "Burro",
-    "Carpa", "Campo", "Cenar", "Costo", "Crudo",
-    "Denso", "Datos", "Dados", "Doble", "Dulce",
+const palabras = ["Amigo", "Aviso", "Ancho", "Arena", "Asado", "arbol",
+    "Banco", "Banda", "BARCO", "Bolsa", "Burro", "barro", "bajar",
+    "Carpa", "Campo", "Cenar", "Costo", "Crudo", "carta", "carro", "cerro", "cobre", "cabra",
+    "Denso", "Datos", "Dados", "Doble", "Dulce", "dardo",
     "Enero", "Error", "Erizo", "Estar", "Echar",
     "Feria", "Fallo", "Filas", "Fuego", "Ficha",
     "Gatos", "Grano", "Globo", "Gordo", "Grave",
-    "Hielo", "Horno", "Hongo", "Humor", "Hacha",
+    "Hielo", "Horno", "Hongo", "Humor", "Hacha", "hacer",
     "Indio", "Islas", "Igual", "Impar", "Idolo",
     "Joven", "Julio", "Japon", "Jugar", "Jarra",
     "Koala", "Karma",
-    "Libro", "Lugar", "Lente", "Largo", "Llave",
+    "Libro", "Lugar", "Lente", "Largo", "Llave", "libre", "luces",
     "Metro", "Manos", "Miedo", "Marco", "Melon",
     "Nubes", "Nueva", "Norte", "Nieve", "Nacer",
     "Oveja", "Opera", "Oruga", "Oliva", "Ondas",
-    "Pecho", "perro", "Poder", "Paris", "Pinza",
+    "Pecho", "perro", "Poder", "Paris", "Pinza", "pollo",
     "Queso", "Quema", "Queda", "Quiso",
-    "Rubio", "Rango", "Radio", "Ronda", "Reloj",
+    "Rubio", "Rango", "Radio", "Ronda", "Reloj", "robar",
     "Secar", "Sobre", "Salsa", "Sello", "Sabio",
     "Trono", "Torre", "Tabla", "Tenis", "Terco",
     "Union", "Unico", "Untar",
@@ -31,7 +29,6 @@ const palabras = ["Amigo", "Aviso", "Ancho", "Arena", "Asado",
 /**
  * Elige la palabra oculta que el usuario tendra que adivinar
  * @method palabraAAdivinar
- * @return palabra oculta aleatoria
  */
 
 palabraAAdivinar = () => {
@@ -40,13 +37,6 @@ palabraAAdivinar = () => {
     localStorage.setItem("PA",palabraAleatoria);
 }
 
-
-
-/*cartelGanaPierde = () => {
-    const almacenado = localStorage.getItem("PA");
-    console.log(almacenado);
-}
-*/
 /**
  * Verifica que la palabra ingresada por el usuario sea valida
  * @method cargarPalabra
@@ -67,7 +57,8 @@ cargarPalabra = () => {
     if (coincide) {
         for (let i = 0; i < palabra.length; i++) {
             letrasIntento[n+i].value = palabra[i];
-            compararPalabra(palabra[i],i);
+            console.log(palabra.length);
+            compararPalabra(palabra);
         }
         contenidoCuadrado.value = "";
         colorCuadrado.style.backgroundColor = "white";
@@ -79,29 +70,49 @@ cargarPalabra = () => {
         colorCuadrado.style.backgroundColor = "white";
     }
 }
+/**
+ * Compara las letras de la palabra valida ingresada con las letras de la palabra a advinar
+ * @method intento - recibe una por una las letras del intento ingresado por el usuario
+ * @method i - indice de la letra
+ */
 
-compararPalabra = (intento, i) => {
+compararPalabra = (intento) => {
+    let iguales = 0;
+    let verde = false;
     const almacenado = localStorage.getItem("PA");
-    console.log(almacenado);
-    for (let j = 0; j < almacenado.length; j++) {
-        if (intento.toUpperCase() === almacenado[j].toUpperCase()) {
-            if (i === j) {
-                document.getElementsByName("letra")[n+i].style.backgroundColor = "#11D147";
-                document.getElementsByClassName("celda")[n+i].style.backgroundColor = "#11D147";
-                iguales++;
-            } else {
-                document.getElementsByName("letra")[n+i].style.backgroundColor = "#FEEA00";
-                document.getElementsByClassName("celda")[n+i].style.backgroundColor = "#FEEA00";
+    console.log(almacenado, almacenado.length);
+    for (let i=0;i<intento.length;i++) {
+        for (let j = 0; j < almacenado.length; j++) {
+            if (intento[i].toUpperCase() === almacenado[j].toUpperCase()) {
+                console.log(almacenado[j], j);
+                console.log(intento[i], i);
+                if (i === j) {
+                    document.getElementsByName("letra")[n + i].style.backgroundColor = "#11D147";
+                    document.getElementsByClassName("celda")[n + i].style.backgroundColor = "#11D147";
+                    iguales++;
+                    break;
+                } else {
+                    document.getElementsByName("letra")[n + i].style.backgroundColor = "#FEEA00";
+                    document.getElementsByClassName("celda")[n + i].style.backgroundColor = "#FEEA00";
+                }
             }
         }
     }
-    if (n===25 && iguales<5){
-        //document.getElementsByClassName("carteles")[0].style.display = 'block'
-        //document.getElementById("perdiste").style.zIndex = 2;
-        document.getElementById("mostrarPalabra").innerHTML = "PALABRA OCULTA: " + almacenado.toUpperCase();
-        document.getElementById("cargar").style.display = 'none';
-        document.getElementById("labelCargar").style.display = 'none';
-    }
+    carteles(iguales, almacenado);
 }
 
-
+carteles = (iguales, almacenado) => {
+    if (n===25 && iguales<5){
+        document.getElementById("perdiste").style.display = 'block'
+        document.getElementById("mostrarPalabra").innerHTML = "PALABRA OCULTA: " + almacenado.toUpperCase();
+        document.getElementById("cargar").style.display = 'none';
+        //document.getElementById("puntaje").style.display = 'none';
+        document.getElementById("labelCargar").style.display = 'none';
+    }
+    else if (iguales === 5){
+        document.getElementById("ganaste").style.display = 'block'
+        document.getElementById("cargar").style.display = 'none';
+        document.getElementById("labelCargar").style.display = 'none';
+        document.getElementById("puntaje").innerHTML += 100;
+    }
+}
