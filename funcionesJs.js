6b079786165986068250f34dfaba33ef97cc6791
aguslopez9 <agustinlopezv99@gmail.com>
2023-05-31 23:32:17 -0300

    canvas

--- funcionesJs.js
+++ funcionesJs.js
@@ -1,7 +1,7 @@
 let n = 0;
 const palabras = ["Amigo", "Aviso", "Ancho", "Arena", "Asado", "arbol",
     "Banco", "Banda", "BARCO", "Bolsa", "Burro", "barro", "bajar",
-    "Carpa", "Campo", "Cenar", "Costo", "Crudo", "carta", "carro", "cerro", "cobre", "cabra", "cuero",
+    "Carpa", "Campo", "Cenar", "Costo", "Crudo", "carta", "carro", "cerro", "cobre", "cabra", "cuero", "curva",
     "Denso", "Datos", "Dados", "Doble", "Dulce", "dardo",
     "Enero", "Error", "Erizo", "Estar", "Echar",
     "Feria", "Fallo", "Filas", "Fuego", "Ficha",
@@ -11,16 +11,16 @@ const palabras = ["Amigo", "Aviso", "Ancho", "Arena", "Asado", "arbol",
     "Joven", "Julio", "Japon", "Jugar", "Jarra", "jabon",
     "Koala", "Karma",
     "Libro", "Lugar", "Lente", "Largo", "Llave", "libre", "luces",
-    "Metro", "Manos", "Miedo", "Marco", "Melon",
+    "Metro", "Manos", "Miedo", "Marco", "Melon", "multa",
     "Nubes", "Nueva", "Norte", "Nieve", "Nacer",
     "Oveja", "Opera", "Oruga", "Oliva", "Ondas",
-    "Pecho", "perro", "Poder", "Paris", "Pinza", "pollo",
+    "Pecho", "perro", "Poder", "Paris", "Pinza", "pollo", "primo",
     "Queso", "Quema", "Queda", "Quiso",
     "Rubio", "Rango", "Radio", "Ronda", "Reloj", "robar",
     "Secar", "Sobre", "Salsa", "Sello", "Sabio", "suelo",
     "Trono", "Torre", "Tabla", "Tenis", "Terco",
     "Union", "Unico", "Untar",
-    "Vuelo", "Voces", "Valor", "Verde", "Velas",
+    "Vuelo", "Voces", "Valor", "Verde", "Velas", "viejo",
     "Wafle",
     "Yendo", "Yates",
     "Zorro", "Zafar", "Zumba", "Zonas", "Zurdo", "zebra"
@@ -32,21 +32,19 @@ const palabras = ["Amigo", "Aviso", "Ancho", "Arena", "Asado", "arbol",
  */
 
 let palabraAAdivinar = () => {
-    let indiceAleatorio = Math.floor(Math.random()*palabras.length);
+    let indiceAleatorio = Math.floor(Math.random() * palabras.length);
     let palabraAleatoria = palabras[indiceAleatorio];
-    localStorage.setItem("PA",palabraAleatoria);
+    localStorage.setItem("PA", palabraAleatoria);
 }
 
 /**
- * Descripción de que hace la función
- * @method Nombre de la función
- * @param {string} ParámetroA - Explicación de que valor almacena ParámetroA
- * @param {number} ParámetroB - Explicación de que valor almacena ParámetroB
- * @return Valor que retorna
+ * Verifica que el intento sea una palabra valida comparandola con todas las palabras del arreglo palabras
+ * @method cargarPalabra
+ * @param {string} intento - palabra ingresada por el usuario
  */
-let cargarPalabra = () => {
+let cargarPalabra = (intento) => {
     let coincide = false;
-    let palabra = document.getElementById("cargar").value.toUpperCase();
+    let palabra = intento.toUpperCase();
     const letrasIntento = document.getElementsByName("letra");
     const colorCuadrado = document.getElementById("cargar");
     const contenidoCuadrado = document.getElementById("cargar");
@@ -58,14 +56,12 @@ let cargarPalabra = () => {
     }
     if (coincide) {
         for (let i = 0; i < palabra.length; i++) {
-            letrasIntento[n+i].value = palabra[i];
-            console.log(palabra.length);
+            letrasIntento[n + i].value = palabra[i];
             compararPalabra(palabra);
         }
         contenidoCuadrado.value = "";
         colorCuadrado.style.backgroundColor = "white";
-        n+=5;
-
+        n += 5;
     } else if (!coincide && palabra.length === 5) {
         colorCuadrado.style.backgroundColor = "red";
     } else {
@@ -73,16 +69,16 @@ let cargarPalabra = () => {
     }
 }
 /**
- * Compara la palabra ingresada por el usuario con la palabra a adivinar
+ * Compara la palabra valida ingresada por el usuario con la palabra a adivinar
  * @method compararPalabra
  * @param {string} intento - Palabra ingresada por el usuario
  */
 
-compararPalabra = (intento) => {
+let compararPalabra = (intento) => {
     let iguales = 0;
     const almacenado = localStorage.getItem("PA");
-    console.log(almacenado, almacenado.length);
-    for (let i=0;i<intento.length;i++) {
+    console.log(almacenado);
+    for (let i = 0; i < intento.length; i++) {
         for (let j = 0; j < almacenado.length; j++) {
             if (intento[i].toUpperCase() === almacenado[j].toUpperCase()) {
                 if (i === j) {
@@ -100,44 +96,48 @@ compararPalabra = (intento) => {
     carteles(iguales, almacenado);
 }
 /**
- * Dependiendo si el jugador encuentra o no la palabra oculta, muestra un cartel diferente
+ * Dependiendo si el jugador encuentra o no la palabra oculta, muestra el cartel correspondiente
  * @method carteles
- * @param {string} almacenado - Explicación de que valor almacena ParámetroA
- * @param {number} iguales - Explicación de que valor almacena ParámetroB
- * @return Valor que retorna
+ * @param {string} almacenado - Palabra a adivinar
+ * @param {number} iguales - Cantidad de caracteres que coinciden en letra como en posicion
  */
 let carteles = (iguales, almacenado) => {
     const cartelPerdiste = document.getElementById("perdiste");
     const cartelGanaste = document.getElementById("ganaste");
     const inputCargar = document.getElementById("cargar");
     const labelCargar = document.getElementById("labelCargar");
-    if (n===25 && iguales<5){
+    if (n === 25 && iguales < 5) {
         cartelPerdiste.style.display = 'block'
         document.getElementById("mostrarPalabra").innerHTML = "PALABRA OCULTA: " + almacenado.toUpperCase();
         inputCargar.style.display = 'none';
         labelCargar.style.display = 'none';
-    }
-    else if (iguales === 5){
+    } else if (iguales === 5) {
         cartelGanaste.style.display = 'block'
         inputCargar.style.display = 'none';
-       labelCargar.style.display = 'none';
+        labelCargar.style.display = 'none';
         puntaje();
     }
 }
 /**
- * Descripción de que hace la función
- * @method Nombre de la función
- * @param {string} ParámetroA - Explicación de que valor almacena ParámetroA
- * @param {number} ParámetroB - Explicación de que valor almacena ParámetroB
- * @return Valor que retorna
+ * Actualiza el puntaje cada vez que el jugador pasa de nivel
+ * @method puntaje
+ * @param {string} puntos - Explicación de que valor almacena ParámetroB
  */
-let puntaje = (puntos) => {
+let puntaje = () => {
+    let puntos =0;
     let puntajeHtml = document.getElementById("puntaje");
-    puntos = parseInt(puntos);
-    puntos = puntos + 100;
-    puntajeHtml.value = puntos.toString();
-
+    puntos = parseInt(puntajeHtml.value);
+    puntos += 20;
+    localStorage.setItem("puntos", puntos.toString());
+    //puntajeHtml.value = puntos.toString();
 }
+
+let mostrarPuntaje = () => {
+    let puntos = localStorage.getItem("puntos");
+    document.getElementsByClassName("puntaje")[0].innerHTML = puntos;
+    document.getElementsByClassName("puntaje")[1].innerHTML = puntos;
+}
+
 /**
  * Descripción de que hace la función
  * @method Nombre de la función
@@ -146,13 +146,9 @@ let puntaje = (puntos) => {
  * @return Valor que retorna
  */
 let botonInicio = () => {
-    if (n<25){
+    if (n < 25) {
         document.getElementById("alInicio").style.display = 'block';
-        document.getElementById("labelCargar").style.display = 'none';
-        document.getElementById("cargar").style.display = 'none';
-
-    }
-    else {
+    } else {
         location.href = "index.html";
     }
 }
@@ -167,28 +163,35 @@ let botonSeguirJugando = () => {
     document.getElementById("alInicio").style.display = 'none';
 }
 /**
-* Descripción de que hace la función
-* @method Nombre de la función
-* @param {string} ParámetroA - Explicación de que valor almacena ParámetroA
-* @param {number} ParámetroB - Explicación de que valor almacena ParámetroB
-* @return Valor que retorna
-*/
-let dibujarCuadrados = () => {
+ * Descripción de que hace la función
+ * @method Nombre de la función
+ * @param {string} ParámetroA - Explicación de que valor almacena ParámetroA
+ * @param {number} ParámetroB - Explicación de que valor almacena ParámetroB
+ * @return Valor que retorna
+ */
+var x = 0;
+var dX = 1;
+var y =0;
+var dY = 1;
+let canvas = () => {
     const canvas = document.getElementById("myCanvas");
     const ctx = canvas.getContext("2d");
+    const altMaxima = canvas.height;
+    const anchoMaximo = canvas.width;
 
-    let xMax = canvas.width;
-    let yMax = canvas.height;
-    ctx.strokeStyle = "white";
-    ctx.fillStyle = "rgba(0, 0, 0, 0)";
-    ctx.lineWidth = 1;
-    //dibujar rectangulo
-    let margen = 5;
-    ctx.strokeRect(5, margen, 50, yMax-margen*2);
-    ctx.strokeRect(60+margen, margen, 50, yMax-margen*2);
-    ctx.strokeRect(120+margen, margen, 50, yMax-margen*2);
-    ctx.strokeRect(180+margen, margen, 50, yMax-margen*2);
-    ctx.strokeRect(240+margen, margen, 50, yMax-margen*2);
+    let img = new Image();
+    img.src = "imagenes/favicon.ico";
 
-
+    img.onload = function () {
+        canvas.width = canvas.width;
+        ctx.drawImage(img, x, y);
+    }
+    x += dX;
+    y += dY;
+    if (x > anchoMaximo - 15 || x < 0) {
+        dX *= -1;
+    }
+    if (y > altMaxima - 15 || y < 0) {
+        dY *= -1;
+    }
 }
